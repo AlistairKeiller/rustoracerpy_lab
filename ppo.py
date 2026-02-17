c42b6b78d0d1218f67f9b04674279854674af179
Alistair Keiller <alistair@keiller.net>
2026-02-17 15:35:09 -0800

    update

--- ppo.py
+++ ppo.py
@@ -3,7 +3,18 @@ import rustoracerpy
 from stable_baselines3 import PPO
 
 env = gym.make("Rustoracer-v0", yaml="maps/berlin.yaml")
-model = PPO("MlpPolicy", env, verbose=1, n_steps=512, batch_size=64, n_epochs=5)
-model.learn(total_timesteps=50_000)
+model = PPO("MlpPolicy", env, verbose=1, n_steps=2048, batch_size=64, n_epochs=10)
+model.learn(total_timesteps=500_000)
 model.save("racer_ppo")
 env.close()
+
+env = gym.make("Rustoracer-v0", yaml="maps/berlin.yaml", render_mode="human")
+model = PPO.load("racer_ppo")
+
+obs, info = env.reset()
+while True:
+    action = model.predict(obs, deterministic=True)[0]
+    obs, reward, terminated, truncated, info = env.step(action)
+    env.render()
+    if terminated or truncated:
+        obs, info = env.reset()
