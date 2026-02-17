1d195e2e7ca63ce902fec3c1624f5adc80b2a71a
Alistair Keiller <alistair@keiller.net>
2026-02-17 11:49:04 -0800

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+import rustoracerpy
+
+# Initialise the environment
+env = gym.make("Rustoracer-v0", yaml="maps/berlin.yaml")
+
+# Reset the environment to generate the first observation
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    # this is where you would insert your policy
+    action = env.action_space.sample()
+
+    # step (transition) through the environment with the action
+    # receiving the next observation, reward and if the episode has terminated or truncated
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    # If the episode has ended then we can reset to start a new episode
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
